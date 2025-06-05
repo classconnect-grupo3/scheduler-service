1f00e12acc1d0ed0ed5b844bdb68560e6d126391
tinchomorilla <mmorilla@fi.uba.ar>
2025-06-05 00:36:08 -0300

    refactor: update MongoClient import and add type hint for get_db function

--- src/database/db.py
+++ src/database/db.py
@@ -1,10 +1,10 @@
-from pymongo import MongoClient
 import os
+from pymongo.mongo_client import MongoClient
+from pymongo.database import Database
 
 MONGO_URL = os.getenv("MONGO_URL")
 client = MongoClient(MONGO_URL)
-db = client.scheduler_db 
+db = client.scheduler_db  
 
-
-def get_db():
+def get_db() -> Database:
     return db
