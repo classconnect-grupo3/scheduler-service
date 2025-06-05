96a54669db976aa7b887b5c7a146215255adfa2f
tinchomorilla <mmorilla@fi.uba.ar>
2025-06-05 13:23:29 -0300

    refactor: update logging messages to English and adjust RabbitMQ queue durability settings

--- src/rabbitmq/publisher.py
+++ src/rabbitmq/publisher.py
@@ -6,20 +6,19 @@ from utils.logger import setup_logger
 
 logger = setup_logger()
 
+
 def publish_reminder_event(event_data: dict):
     connection = get_rabbitmq_connection()
     channel = connection.channel()
     queue_name = os.getenv("NOTIFICATIONS_QUEUE_NAME")
-    channel.queue_declare(queue=queue_name, durable=True)
-    
-    logger.info("Publicando evento")
+    channel.queue_declare(queue=queue_name, durable=False)
+
+    logger.info("📨 Publishing reminder event")
     channel.basic_publish(
-        exchange='',
+        exchange="",
         routing_key=queue_name,
         body=json.dumps(event_data),
-        properties=pika.BasicProperties(
-            delivery_mode=2  # persistente
-        )
+        properties=pika.BasicProperties(delivery_mode=1),  # non-persistent
     )
     connection.close()
-    logger.info("Conexion con RABBITMQ cerrada")
+    logger.info("🔌 RabbitMQ connection closed")
