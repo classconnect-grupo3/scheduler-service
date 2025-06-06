a455af72c16a816a827b95eeb414f02ab6a7544f
tinchomorilla <mmorilla@fi.uba.ar>
2025-06-06 18:11:13 -0300

    feat: add get_reminder_type function to determine "reminder type" based on due date

---
+++ src/scheduler/get_reminder_type.py
@@ -0,0 +1,13 @@
+from schemas.reminder_type import ReminderType, TimeRanges
+from sqlalchemy.orm import Session
+from datetime import datetime, UTC
+
+
+def get_reminder_type(due_date: datetime, now: datetime) -> str | None:
+    delta_hours = (due_date - now).total_seconds() / 3600
+
+    if TimeRanges.HOUR_24_MIN <= delta_hours <= TimeRanges.HOUR_24_MAX:
+        return ReminderType.HOUR_24
+    elif TimeRanges.HOUR_1_MIN <= delta_hours <= TimeRanges.HOUR_1_MAX:
+        return ReminderType.HOUR_1
+    return None
