a6f9faf2e9873be42a1e0901467130a4f16fb960
tinchomorilla <mmorilla@fi.uba.ar>
2025-06-06 18:11:45 -0300

    feat: add ReminderType and TimeRanges classes for reminder scheduling constants

---
+++ src/schemas/reminder_type.py
@@ -0,0 +1,12 @@
+# Reminder type constants
+class ReminderType:
+    HOUR_24 = "24h"
+    HOUR_1 = "1h"
+
+
+# Time range constants (in hours)
+class TimeRanges:
+    HOUR_24_MIN = 23.5
+    HOUR_24_MAX = 24.5
+    HOUR_1_MIN = 0.5
+    HOUR_1_MAX = 1.5
